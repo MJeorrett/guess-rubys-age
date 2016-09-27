a96aa62dbe02bc88e15861477dd451a7dee63b30
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-09-27 11:35:44 +0100

    added to_s to age rather than to_i on input

--- guess-rubys-age.rb
+++ guess-rubys-age.rb
@@ -1,9 +1,9 @@
 RUBY_AGE = 1995
 
 puts "Guess the year that Ruby was born: "
-age_guess = gets.chomp.to_i
+age_guess = gets.chomp
 
-if age_guess == RUBY_AGE
+if age_guess == RUBY_AGE.to_s
   puts "Well done that is correct!"
 else
   puts "Sorry that is not correct, she was born in #{RUBY_AGE}"
